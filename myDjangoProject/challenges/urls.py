c387a1737ba9a06074b16f3f5194dd3ed9fbe407
gio <56406617+mikoBerries@users.noreply.github.com>
2023-08-10 14:58:16 +0700

    Ft/templates (#2)
    * working with django templates
    
    * giving css styling for template html

--- myDjangoProject/challenges/urls.py
+++ myDjangoProject/challenges/urls.py
@@ -7,7 +7,7 @@ from . import views
 
 # mapping all routes for this module
 urlpatterns = [
-    path("", views.index),  # /challenges/
+    path("", views.index, name="index"),  # /challenges/
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
