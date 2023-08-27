fab79bdb93b733126fa9a60a279732a34e47ac4e
gio <56406617+mikoBerries@users.noreply.github.com>
2023-08-27 22:38:37 +0700

    Ft/blogs version2 (#7)
    * updating blog with database data
    
    * fix css

---
+++ myDjangoProject/blog/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.core.validators import MinLengthValidator
+
+
+class Tag(models.Model):
+    " Tags for blog"
+    caption = models.CharField(max_length=50)
+
+
+class Author(models.Model):
+    " Author of blogs"
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+
+
+class Post(models.Model):
+    " Post data in blogs "
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+    tags = models.ManyToManyField(Tag)
