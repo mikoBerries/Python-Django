feb54016bc95585713dd86ba1bf814be843cc44f
gio <56406617+mikoBerries@users.noreply.github.com>
2023-08-29 23:29:14 +0700

    setting and extract sessiong using django and django view

--- myDjangoProject/reviews/views.py
+++ myDjangoProject/reviews/views.py
@@ -140,6 +140,17 @@ class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
 
+    def get_context_data(self, **kwargs):
+        " Extrating session data and populate in context"
+        loaded_review = self.object
+        request = self.request
+
+        favorite_id = request.session.get("favorite_review")
+
+        context = super().get_context_data(**kwargs)
+        context["is_favorite"] = favorite_id == str(loaded_review.id)
+        return context
+
     # def get_context_data(self, **kwargs):
     #     " Overrading to adding context data to template"
     #     context = super().get_context_data(**kwargs)
@@ -147,3 +158,16 @@ class SingleReviewView(DetailView):
     #     selected_review = Review.objects.get(pk=review_id)
     #     context["review"] = selected_review
     #     return context
+
+
+class AddFavoriteView(View):
+    " view for adding favorite review into user session"
+
+    def post(self, request):
+        review_id = request.POST["review_id"]
+        # fav_review = Review.objects.get(pk=review_id)
+
+        # manipulatin cookies in request
+        request.session["favorite_review"] = review_id
+
+        return HttpResponseRedirect("/reviews/reviews/"+review_id)
